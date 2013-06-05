ba100c6a03e6239a843d89e7e6e88301b39c84fb
Vasileios Trigonakis <vasileios.trigonakis@epfl.ch>
2013-06-05 11:38:07 +0200

    updated run_ll_sl_ht.sh script

--- scripts/run_ll_sl_ht.sh
+++ scripts/run_ll_sl_ht.sh
@@ -2,40 +2,39 @@
 
 ## global  --------------------------------------------------------------------------------------------------------
 out_folder="./data";
-cores="all";
+cores="1 48";
 duration="1000";
 
 
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ## executables ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-lls='./bin/lb-ll "./bin/lb-ll -x2" ./bin/lf-ll'
+lls='./bin/lb-ll "./bin/lb-ll -x2" ./bin/lf-ll' # -x2 does not work
 sls='./bin/lb-sl ./bin/lf-sl'
-hts='./bin/lb-ht "./bin/lb-ht -x2" ./bin/lf-ht'
-## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-## update ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-update=0;
+hts='./bin/lb-ht "./bin/lb-ht -x2" ./bin/lf-ht' # -x2 does not work
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 ## settings --------------------------------------------------------------------------------------------------------
 initials="1024 16536";
+updates="0 20 50";
 load_factors="2 8"
 
 for initial in $initials; 
 do
     range=$((2*$initial));
 
-    out="$out_folder/ll.i$initial.u$update.dat";
-    ./scripts/scalability3.sh "$cores" $lls -d$duration -i$initial -r$range -u$update | tee $out;
-    out="$out_folder/sl.i$initial.u$update.dat";
-    ./scripts/scalability2.sh "$cores" $sls -d$duration -i$initial -r$range -u$update | tee $out;
+    for update in $updates;
+    do
+	out="$out_folder/ll.i$initial.u$update.dat";
+	./scripts/scalability3.sh "$cores" ./bin/lb-ll "./bin/lb-ll -x2" ./bin/lf-ll -d$duration -i$initial -r$range -u$update | tee $out;
+	out="$out_folder/sl.i$initial.u$update.dat";
+	./scripts/scalability2.sh "$cores" $sls -d$duration -i$initial -r$range -u$update | tee $out;
 
-    for load_factor in $load_factors; do
-	out="$out_folder/ht.i$initial.l$load_factor.u$update.dat";
-	./scripts/scalability3.sh "$cores" $hts -d$duration -i$initial -r$range -u$update -l$load_factor | tee $out;
+	for load_factor in $load_factors; 
+	do
+	    out="$out_folder/ht.i$initial.l$load_factor.u$update.dat";
+	    ./scripts/scalability3.sh "$cores" ./bin/lb-ht "./bin/lb-ht -x2" ./bin/lf-ht -d$duration -i$initial -r$range -u$update -l$load_factor | tee $out;
+	done
     done
 done
 
