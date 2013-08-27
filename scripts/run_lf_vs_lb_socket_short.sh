1c4ee367a5ad0caa1ae9ef1549060819b434a520
Vasileios Trigonakis <vasileios.trigonakis@epfl.ch>
2013-08-27 17:51:46 +0200

    removed the haswell specific parts

--- scripts/run_lf_vs_lb_socket_short.sh
+++ scripts/run_lf_vs_lb_socket_short.sh
@@ -7,6 +7,12 @@ printf "" > $out_file;
 initials="8 64 512 1024 2048 8192";
 updates="0 1 10 100";
 
+lb_ll_gl="./bin/lb-ll_gl_ticket -x2";
+if [ $(uname -n) = "diassrv8" ];
+then
+    lb_ll_gl="./bin/lb-ll_gl_hticket -x2";
+fi;
+
 for i in $initials
 do
     for u in $updates
@@ -14,6 +20,6 @@ do
 	r=$((2*$i));	
 	settings="-i$i -r$r -u$u";
 	echo "## $settings" | tee -a $out_file;
-	./scripts/scalability9.sh socket ./bin/lf-ll "./bin/lb-ll_ticket -x2" "./bin/lb-ll_gl_ticket -x2" ./bin/lf-sl ./bin/lb-sl_ticket ./bin/lf-ht "./bin/lb-ht_ticket -x2" "./bin/lb-ht_gl_ticket -x2" "./bin/lb-ht_gl_hticket -x2"  $settings -l4 | tee -a $out_file;
+	./scripts/scalability8.sh socket ./bin/lf-ll "./bin/lb-ll_ticket -x2" "$lb_ll_gl" ./bin/lf-sl ./bin/lb-sl_ticket ./bin/lf-ht "./bin/lb-ht_ticket -x2" "./bin/lb-ht_gl_ticket -x2"  $settings -l4 | tee -a $out_file;
     done;
 done;
