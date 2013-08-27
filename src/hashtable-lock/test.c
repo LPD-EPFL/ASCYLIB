34231453c3f6069638c7775159d4383bc5d1326b
Vasileios Trigonakis <vasileios.trigonakis@epfl.ch>
2013-08-27 16:26:19 +0200

    less prints

--- src/hashtable-lock/test.c
+++ src/hashtable-lock/test.c
@@ -107,6 +107,8 @@ typedef ALIGNED(64) struct thread_data
 } thread_data_t;
 
 
+extern __thread size_t num_pause, num_total, num_xtest;
+
 void*
 test(void *data) 
 {
@@ -247,6 +249,8 @@ test(void *data)
     }
 	
   PF_PRINT;
+ printf("[%d] tot: %10lu / pause: %10lu / ratio: %5.2f / xtests: %lu / ratio: %5.2f\n",
+	d->id, num_total, num_pause, 1.0*num_pause/num_total, num_xtest, 1.0*num_xtest/num_total);
 	
   return NULL;
 }
@@ -568,6 +572,7 @@ main(int argc, char **argv)
     printf("    #removed  : %lu\n", data[i].nb_removed);
     printf("  #contains   : %lu\n", data[i].nb_contains);
     printf("    #found    : %lu\n", data[i].nb_found);
+#if defined(STM)
     printf("  #move       : %lu\n", data[i].nb_move);
     printf("  #moved      : %lu\n", data[i].nb_moved);
     printf("  #snapshot   : %lu\n", data[i].nb_snapshot);
@@ -580,6 +585,7 @@ main(int argc, char **argv)
     printf("    #val-c    : %lu\n", data[i].nb_aborts_validate_commit);
     printf("    #inv-mem  : %lu\n", data[i].nb_aborts_invalid_memory);
     printf("  Max retries : %lu\n", data[i].max_retries);
+#endif
     aborts += data[i].nb_aborts;
     aborts_locked_read += data[i].nb_aborts_locked_read;
     aborts_locked_write += data[i].nb_aborts_locked_write;
@@ -622,6 +628,7 @@ main(int argc, char **argv)
 	   duration);
   } else printf("%lu (%f / s)\n", updates, updates * 1000.0 / duration);
 	
+#if defined(STM)
   printf("#move txs     : %lu (%f / s)\n", moves, moves * 1000.0 / duration);
   printf("  #moved      : %lu (%f / s)\n", moved, moved * 1000.0 / duration);
   printf("#snapshot txs : %lu (%f / s)\n", snapshots, snapshots * 1000.0 / duration);
@@ -634,7 +641,7 @@ main(int argc, char **argv)
   printf("  #val-c      : %lu (%f / s)\n", aborts_validate_commit, aborts_validate_commit * 1000.0 / duration);
   printf("  #inv-mem    : %lu (%f / s)\n", aborts_invalid_memory, aborts_invalid_memory * 1000.0 / duration);
   printf("Max retries   : %lu\n", max_retries);
-	
+#endif	
   /* Delete set */
   ht_delete(set);
 	
