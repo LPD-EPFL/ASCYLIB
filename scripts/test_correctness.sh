44d7c2d46678ddf6ecb22eb792719c1d9d89dd3d
Vasileios Trigonakis <vasileios.trigonakis@epfl.ch>
2013-08-28 16:29:02 +0200

    added $run_script to execute

--- scripts/test_correctness.sh
+++ scripts/test_correctness.sh
@@ -6,9 +6,9 @@ source scripts/config;
 for bin in $(ls ./bin/*);
 do
     echo "Testing: $bin";
-    $bin -n$max_cores -l4 | grep "expected";
-    $bin -n$max_cores -l4 -i32 -r64 | grep "expected";
-    $bin -n$max_cores -l2 -i16 -r32 -u100 | grep "expected";
+    $run_script $bin -n$max_cores -l4 | grep "expected";
+    $run_script $bin -n$max_cores -l4 -i32 -r64 | grep "expected";
+    $run_script $bin -n$max_cores -l2 -i16 -r32 -u100 | grep "expected";
 done;
 
 source scripts/unlock_exec;
