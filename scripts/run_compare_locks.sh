862210b1433c1d531d42397e1117c02ab92dc1be
Vasileios Trigonakis <vasileios.trigonakis@epfl.ch>
2013-08-28 20:00:52 +0200

    printing the settings

--- scripts/run_compare_locks.sh
+++ scripts/run_compare_locks.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 p="lb-ll -x2 -u10";
+echo "## settings: $p";
 ./scripts/run_compare.sh $p
 p="lb-ll -x2 -u10 -i128 -r256";
+echo "## settings: $p";
 ./scripts/run_compare.sh $p
 p="lb-sl  -u10";
+echo "## settings: $p";
 ./scripts/run_compare.sh $p
 p="-u10 -i128 -r256";
+echo "## settings: $p";
 ./scripts/run_compare.sh lb-sl $p
 p="-u10 -l4";
+echo "## settings: $p";
 ./scripts/run_compare.sh lb-ht $p
 p="-u10 -l4 -x2"
+echo "## settings: $p";
 ./scripts/run_compare.sh lb-ht $p
 
