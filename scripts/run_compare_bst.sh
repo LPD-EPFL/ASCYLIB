3cb045cf616d25029ae00cd1a5f0c42e43657010
Igor Zablotchi <zablotch@lpd48core.epfl.ch>
2014-01-04 13:35:42 +0100

    Modified test scripts

--- scripts/run_compare_bst.sh
+++ scripts/run_compare_bst.sh
@@ -4,7 +4,7 @@ out_file="data/compare_bst_"$(date | awk '// {print $2"_"$3}')".dat";
 echo "Output file: $out_file";
 printf "" > $out_file;
 
-initials="64 512 1024 2048 8192"  #"8 64 512 1024 2048 8192";
+initials="1024 8192 131072 1048576"  #"8 64 512 1024 2048 8192";
 updates="0 1 10 100";
 
 for i in $initials
@@ -14,7 +14,7 @@ do
 	r=$((2*$i));	
 	settings="-i$i -r$r -u$u";
 	echo "## $settings" | tee -a $out_file;
-	./scripts/scalability3.sh socket ./bin/lf-bst ./bin/lf-bst-howley ./bin/lb-bst2 $settings | tee -a $out_file;
+	./scripts/scalability6.sh socket ./bin/lf-bst ./bin/lf-bst-howley ./bin/lb-bst-mutex ./bin/lb-bst-ticket ./bin/lb-bst-tas ./bin/lb-bst-spin $settings | tee -a $out_file;
     done;
 done;
 
