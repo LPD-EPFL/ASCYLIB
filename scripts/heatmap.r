5c55bd7059e37dcf7a73df39c325fcafc98a413e
Tudor David <tudor.david@epfl.ch>
2014-01-30 16:40:32 +0100

    added r plot file and a script to run tests for all data structures

---
+++ scripts/heatmap.r
@@ -0,0 +1,19 @@
+#https://github.com/VikParuchuri/simpsons-scripts/blob/master/analyze_sentiment.R
+if (!require("RColorBrewer")) {
+install.packages("RColorBrewer")
+library(RColorBrewer)
+}
+args <- commandArgs()
+print(args[7])
+ll <- read.csv(args[5],sep=",",check.names=FALSE)
+row.names(ll) <- ll$Range
+ll <- ll[,2:6]
+ll_matrix <- data.matrix(ll)
+col = brewer.pal(11,"RdYlGn")
+col2 = brewer.pal(ncol(ll_matrix),"RdYlGn")
+myBreaks=c(0, 33, 50, 75, 85, 97, 103, 110, 130, 150, 200, 1000)
+#now to save in pdf
+pdf(file=args[6])
+#ll_heatmap <- heatmap(ll_matrix, Rowv=NA, Colv=NA, col=col, scale="column", margins=c(5,10))
+ll_heatmap <- heatmap(ll_matrix, Rowv=NA, Colv=NA, col=col, scale="none", ylab=args[9], xlab=args[8], margins=c(5,5), ColSideColors=col2, main=args[7], breaks=myBreaks)
+dev.off()
