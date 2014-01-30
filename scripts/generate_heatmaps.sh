e4429ec8f05c27a241137d68d4766e9addc06997
Tudor David <tudor.david@epfl.ch>
2014-01-30 17:40:34 +0100

    added bst to generate_heatmaps script

--- scripts/generate_heatmaps.sh
+++ scripts/generate_heatmaps.sh
@@ -4,7 +4,7 @@ core=$1
 ./scripts/heatmap.sh ./bin/lb-ll ./bin/lf-ll u s -n${core} -d1000 > data/ll_heatmap_${core}.csv
 ./scripts/heatmap.sh ./bin/lb-ht ./bin/lf-ht u s -n${core} -d1000 > data/ht_heatmap_${core}.csv
 ./scripts/heatmap.sh ./bin/lb-sl ./bin/lf-sl u s -n${core} -d1000 > data/sl_heatmap_${core}.csv
-#./scripts/heatmap.sh ./bin/lb-bst ./bin/lf-bst u s -n${core} -d1000 > data/bst_heatmap_${core}.csv
+./scripts/heatmap.sh ./bin/lb-bst2 ./bin/lf-bst u s -n${core} -d1000 > data/bst_heatmap_${core}.csv
 
 #run the next only if R is installed
 #r -f scripts/heatmap.r --args data/ll_heatmap_${core}.csv data/ll_heatmap_${core}.pdf "Linked list, lf/lb" "Key range" "Update ratio"
