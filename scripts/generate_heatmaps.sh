416e7cec0f1e23326c0f46e119d7092e15103ae8
Tudor David <tudor.david@epfl.ch>
2014-01-31 16:38:12 +0100

    small update

--- scripts/generate_heatmaps.sh
+++ scripts/generate_heatmaps.sh
@@ -6,13 +6,14 @@ fi
 
 core=$1
 
-./scripts/heatmap.sh ./bin/lb-ll ./bin/lf-ll u s -n${core} -d1000 > data/ll_heatmap_${core}.csv
-./scripts/heatmap.sh ./bin/lb-ht ./bin/lf-ht u s -n${core} -d1000 > data/ht_heatmap_${core}.csv
-./scripts/heatmap.sh ./bin/lb-sl ./bin/lf-sl u s -n${core} -d1000 > data/sl_heatmap_${core}.csv
-./scripts/heatmap.sh ./bin/lb-bst2 ./bin/lf-bst u s -n${core} -d1000 > data/bst_heatmap_${core}.csv
+unm=$(uname -n);
+./scripts/heatmap.sh ./bin/lb-ll ./bin/lf-ll u s -n${core} -d1000 > data/${unm}_ll_heatmap_${core}.csv
+./scripts/heatmap.sh ./bin/lb-ht ./bin/lf-ht u s -n${core} -d1000 > data/${unm}_ht_heatmap_${core}.csv
+./scripts/heatmap.sh ./bin/lb-sl ./bin/lf-sl u s -n${core} -d1000 > data/${unm}_sl_heatmap_${core}.csv
+./scripts/heatmap.sh ./bin/lb-bst2 ./bin/lf-bst u s -n${core} -d1000 > data/${unm}_bst_heatmap_${core}.csv
 
 #run the next only if R is installed
-#r -f scripts/heatmap.r --args data/ll_heatmap_${core}.csv data/ll_heatmap_${core}.pdf "Linked list, lf/lb" "Key range" "Update ratio"
-#r -f scripts/heatmap.r --args data/sl_heatmap_${core}.csv data/sl_heatmap_${core}.pdf "Skip-list, lf/lb" "Key range" "Update ratio"
-#r -f scripts/heatmap.r --args data/ht_heatmap_${core}.csv data/ht_heatmap_${core}.pdf "Hash table, lf/lb" "Key range" "Update ratio"
-#r -f scripts/heatmap.r --args data/bst_heatmap_${core}.csv data/bst_heatmap_${core}.pdf "BST, lf/lb" "Key range" "Update ratio"
+#r -f scripts/heatmap.r --args data/${unm}_ll_heatmap_${core}.csv data/${unm}_ll_heatmap_${core}.pdf "Linked list, lf/lb" "Key range" "Update ratio"
+#r -f scripts/heatmap.r --args data/${unm}_sl_heatmap_${core}.csv data/${unm}_sl_heatmap_${core}.pdf "Skip-list, lf/lb" "Key range" "Update ratio"
+#r -f scripts/heatmap.r --args data/${unm}_ht_heatmap_${core}.csv data/${unm}_ht_heatmap_${core}.pdf "Hash table, lf/lb" "Key range" "Update ratio"
+#r -f scripts/heatmap.r --args data/${unm}_bst_heatmap_${core}.csv data/${unm}_bst_heatmap_${core}.pdf "BST, lf/lb" "Key range" "Update ratio"
