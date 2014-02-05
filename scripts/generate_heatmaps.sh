83ab365c9777d2cfb96dd29410fb57ce27c14f0d
Tudor David <tudor.david@epfl.ch>
2014-02-05 15:38:55 +0100

    added script to create pdfs heatmaps; updatead the r script to show the row labels correctly

--- scripts/generate_heatmaps.sh
+++ scripts/generate_heatmaps.sh
@@ -6,14 +6,13 @@ fi
 
 core=$1
 
-unm=$(uname -n);
-./scripts/heatmap.sh ./bin/lb-ll ./bin/lf-ll u s -n${core} -d1000 > data/${unm}_ll_heatmap_${core}.csv
-./scripts/heatmap.sh ./bin/lb-ht ./bin/lf-ht u s -n${core} -d1000 > data/${unm}_ht_heatmap_${core}.csv
-./scripts/heatmap.sh ./bin/lb-sl ./bin/lf-sl u s -n${core} -d1000 > data/${unm}_sl_heatmap_${core}.csv
-./scripts/heatmap.sh ./bin/lb-bst2 ./bin/lf-bst u s -n${core} -d1000 > data/${unm}_bst_heatmap_${core}.csv
+unames="maglite lpd48core lpdxeon2680 parsasrv1.epfl.ch"
 
-#run the next only if R is installed
-#r -f scripts/heatmap.r --args data/${unm}_ll_heatmap_${core}.csv data/${unm}_ll_heatmap_${core}.pdf "Linked list, lf/lb" "Key range" "Update ratio"
-#r -f scripts/heatmap.r --args data/${unm}_sl_heatmap_${core}.csv data/${unm}_sl_heatmap_${core}.pdf "Skip-list, lf/lb" "Key range" "Update ratio"
-#r -f scripts/heatmap.r --args data/${unm}_ht_heatmap_${core}.csv data/${unm}_ht_heatmap_${core}.pdf "Hash table, lf/lb" "Key range" "Update ratio"
-#r -f scripts/heatmap.r --args data/${unm}_bst_heatmap_${core}.csv data/${unm}_bst_heatmap_${core}.pdf "BST, lf/lb" "Key range" "Update ratio"
+
+for unm in $unames
+do
+r -f heatmap.r --args ${unm}_ll_heatmap_${core}.csv ${unm}_ll_heatmap_${core}.pdf "Linked list, lf/lb, $unm, $core cores" "Key range" "Update ratio"
+r -f heatmap.r --args ${unm}_sl_heatmap_${core}.csv ${unm}_sl_heatmap_${core}.pdf "Skip-list, lf/lb, $unm, $core cores" "Key range" "Update ratio"
+r -f heatmap.r --args ${unm}_ht_heatmap_${core}.csv ${unm}_ht_heatmap_${core}.pdf "Hash table, lf/lb, $unm, $core cores" "Key range" "Update ratio"
+r -f heatmap.r --args ${unm}_bst_heatmap_${core}.csv ${unm}_bst_heatmap_${core}.pdf "BST, lf/lb, $unm, $core cores" "Key range" "Update ratio"
+done
--- scripts/heatmap.r
+++ scripts/heatmap.r
@@ -6,12 +6,12 @@ library(RColorBrewer)
 args <- commandArgs()
 print(args[7])
 ll <- read.csv(args[5],sep=",",check.names=FALSE)
-row.names(ll) <- ll$Range
-ll <- ll[,2:6]
+row.names(ll) <- ll[,1]
+ll <- ll[,2:12]
 ll_matrix <- data.matrix(ll)
 col = brewer.pal(11,"RdYlGn")
 col2 = brewer.pal(ncol(ll_matrix),"RdYlGn")
-myBreaks=c(0, 33, 50, 75, 85, 97, 103, 110, 130, 150, 200, 1000)
+myBreaks=c(0, 33, 50, 75, 85, 97, 103, 110, 130, 150, 200, 100000)
 #now to save in pdf
 pdf(file=args[6])
 #ll_heatmap <- heatmap(ll_matrix, Rowv=NA, Colv=NA, col=col, scale="column", margins=c(5,10))
