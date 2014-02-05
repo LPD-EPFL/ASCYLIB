cfe6b658ee2bbd49ce97a27b2a8b433a4363947f
Tudor David <tudor.david@epfl.ch>
2014-02-05 12:23:49 +0100

    updated heatmap generation script; axes values are now printed

--- scripts/heatmap.sh
+++ scripts/heatmap.sh
@@ -41,12 +41,15 @@ source scripts/lock_exec;
 case "${y_axis}" in
 s)  quantity_1=${ranges}
     par_1="-r"
+    par_1_name="range"
     ;;
 u)  quantity_1=${updates}
     par_1="-u"
+    par_1_name="updates"
     ;;
 c)  quantity_1=${cores}
     par_1="-n"
+    par_1_name="cores"
     ;;
 *) echo "Format: ./heatmap.sh prog_1 prog_2 y_axis x_axis other_parameters"
     exit;
@@ -56,20 +59,32 @@ esac
 case "${x_axis}" in
 s)  quantity_2=${ranges}
     par_2="-r"
+    par_2_name="range"
     ;;
 u)  quantity_2=${updates}
     par_2="-u"
+    par_2_name="updates"
     ;;
 c)  quantity_2=${cores}
     par_2="-n"
+    par_2_name="cores"
     ;;
 *) echo "Format: ./heatmap.sh prog_1 prog_2 y_axis x_axis other_parameters"
     exit;
    ;;
 esac
 
+printf "%s" ${par_1_name}
+printf "/%s," ${par_2_name}
+for x in ${quantity_2}
+do
+    printf "%-8.0f," $x;
+done
+printf "\n" 
+
 for y in ${quantity_1}
 do
+printf "%-8.0f," $y
 for x in ${quantity_2}
 do
     init=""
