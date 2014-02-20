19608012b539b7d1f0ebe915176f2b25bfb4e8da
Vasileios Trigonakis <vasileios.trigonakis@epfl.ch>
2014-02-20 15:56:46 +0100

    more granular printing

--- scripts/heatmap_c_u.sh
+++ scripts/heatmap_c_u.sh
@@ -8,6 +8,7 @@ then
 fi
 
 inits="256 1024 2048 8129 65536"
+duration=2000;
 
 for initial in ${inits}
 do
@@ -17,8 +18,12 @@ echo "## initial: $initial";
 
 unm=$(uname -n);
 rm data/${unm}_*_heatmap_uc_${initial}.csv
-./scripts/heatmap.sh ./bin/lb-ll ./bin/lf-ll u c -i${initial} -r${range} -d2000 | tee data/${unm}_ll_heatmap_uc_${initial}.csv
-./scripts/heatmap.sh ./bin/lb-ht_gl ./bin/lf-ht u c -i${initial} -r${range} -d2000 | tee data/${unm}_ht_heatmap_uc_${initial}.csv
-./scripts/heatmap.sh ./bin/lb-sl ./bin/lf-sl u c -i${initial} -r${range} -d2000 | tee data/${unm}_sl_heatmap_uc_${initial}.csv
-./scripts/heatmap.sh ./bin/lb-bst2 ./bin/lf-bst-howley u c -i${initial} -r${range} -d2000 | tee data/${unm}_bst_heatmap_uc_${initial}.csv
+echo '#  ll';
+./scripts/heatmap.sh ./bin/lb-ll ./bin/lf-ll u c -i${initial} -r${range} -d$duration | tee data/${unm}_ll_heatmap_uc_${initial}.csv
+echo '#  ht';
+./scripts/heatmap.sh ./bin/lb-ht_gl ./bin/lf-ht u c -i${initial} -r${range} -d$duration | tee data/${unm}_ht_heatmap_uc_${initial}.csv
+echo '#  sl';
+./scripts/heatmap.sh ./bin/lb-sl ./bin/lf-sl u c -i${initial} -r${range} -d$duration | tee data/${unm}_sl_heatmap_uc_${initial}.csv
+echo '#  bst';
+./scripts/heatmap.sh ./bin/lb-bst2 ./bin/lf-bst-howley u c -i${initial} -r${range} -d$duration | tee data/${unm}_bst_heatmap_uc_${initial}.cs
 done
