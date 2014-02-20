d8bf682f7311990f74b0c8b7fd99994a6fc5953f
Tudor David <tudor.david@epfl.ch>
2014-02-20 16:34:23 +0100

    updated scripts

--- scripts/cu_heatmaps.sh
+++ scripts/cu_heatmaps.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 unames="maglite lpd48core lpdxeon2680 parsasrv1.epfl.ch diassrv8"
-inits="256 1024 2048 8129 65536"
+inits="256 1024 2048 8192 65536"
 for unm in $unames
 do
 for initial in ${inits}
 do
 
-r -f ./scripts/heatmap.r --args ./data/${unm}_ll_heatmap_uc_${initial}.csv ./data/${unm}_ll_heatmap_uc_${initial}.pdf "Linked list, lf/lb, $unm, $initial initial" "Cores" "Update ratio"
-r -f ./scripts/heatmap.r --args ./data/${unm}_ht_heatmap_uc_${initial}.csv ./data/${unm}_ht_heatmap_uc_${initial}.pdf "Hash table, lf/lb, $unm, $initial initial" "Cores" "Update ratio"
-r -f ./scripts/heatmap.r --args ./data/${unm}_sl_heatmap_uc_${initial}.csv ./data/${unm}_sl_heatmap_uc_${initial}.pdf "Skip list, lf/lb, $unm, $initial initial" "Cores" "Update ratio"
-r -f ./scripts/heatmap.r --args ./data/${unm}_bst_heatmap_uc_${initial}.csv ./data/${unm}_bst_heatmap_uc_${initial}.pdf "BST, lf/lb, $unm, $initial initial" "Cores" "Update ratio"
+R -f ./scripts/heatmap.r --args ./data/${unm}_ll_heatmap_uc_${initial}.csv ./data/${unm}_ll_heatmap_uc_${initial}.pdf "Linked list, lf/lb, $unm, $initial initial" "Cores" "Update ratio"
+R -f ./scripts/heatmap.r --args ./data/${unm}_ht_heatmap_uc_${initial}.csv ./data/${unm}_ht_heatmap_uc_${initial}.pdf "Hash table, lf/lb, $unm, $initial initial" "Cores" "Update ratio"
+R -f ./scripts/heatmap.r --args ./data/${unm}_sl_heatmap_uc_${initial}.csv ./data/${unm}_sl_heatmap_uc_${initial}.pdf "Skip list, lf/lb, $unm, $initial initial" "Cores" "Update ratio"
+R -f ./scripts/heatmap.r --args ./data/${unm}_bst_heatmap_uc_${initial}.csv ./data/${unm}_bst_heatmap_uc_${initial}.pdf "BST, lf/lb, $unm, $initial initial" "Cores" "Update ratio"
 done
 done
