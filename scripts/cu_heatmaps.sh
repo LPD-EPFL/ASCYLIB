d7a8b81cf5ff71475b630b317010843013c3f022
Vasileios Trigonakis <vasileios.trigonakis@epfl.ch>
2014-02-20 16:41:54 +0100

    made it to run on for existing csvs

--- scripts/cu_heatmaps.sh
+++ scripts/cu_heatmaps.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 unames="maglite lpd48core lpdxeon2680 parsasrv1.epfl.ch diassrv8"
 inits="256 1024 2048 8192 65536"
+
+plots_folder=./plots
+[ -d foo ] || mkdir $plots_folder;
+
 for unm in $unames
 do
-for initial in ${inits}
-do
+    for initial in ${inits}
+    do
 
-R -f ./scripts/heatmap.r --args ./data/${unm}_ll_heatmap_uc_${initial}.csv ./data/${unm}_ll_heatmap_uc_${initial}.pdf "Linked list, lf/lb, $unm, $initial initial" "Cores" "Update ratio"
-R -f ./scripts/heatmap.r --args ./data/${unm}_ht_heatmap_uc_${initial}.csv ./data/${unm}_ht_heatmap_uc_${initial}.pdf "Hash table, lf/lb, $unm, $initial initial" "Cores" "Update ratio"
-R -f ./scripts/heatmap.r --args ./data/${unm}_sl_heatmap_uc_${initial}.csv ./data/${unm}_sl_heatmap_uc_${initial}.pdf "Skip list, lf/lb, $unm, $initial initial" "Cores" "Update ratio"
-R -f ./scripts/heatmap.r --args ./data/${unm}_bst_heatmap_uc_${initial}.csv ./data/${unm}_bst_heatmap_uc_${initial}.pdf "BST, lf/lb, $unm, $initial initial" "Cores" "Update ratio"
-done
+	data=./data/${unm}_ll_heatmap_uc_${initial}.csv;
+	[ -f $data ] && R -f ./scripts/heatmap.r --args $data ./$(plots_folder)/${unm}_ll_heatmap_uc_${initial}.pdf "Linked list, lf/lb, $unm, $initial initial" "Cores" "Update ratio"
+	data=./data/${unm}_ht_heatmap_uc_${initial}.csv;
+	[ -f $data ] && R -f ./scripts/heatmap.r --args $data ./$(plots_folder)/${unm}_ht_heatmap_uc_${initial}.pdf "Hash table, lf/lb, $unm, $initial initial" "Cores" "Update ratio"
+	data=./data/${unm}_sl_heatmap_uc_${initial}.csv;
+	[ -f $data ] && R -f ./scripts/heatmap.r --args $data ./$(plots_folder)/${unm}_sl_heatmap_uc_${initial}.pdf "Skip list, lf/lb, $unm, $initial initial" "Cores" "Update ratio"
+	data=./data/${unm}_bst_heatmap_uc_${initial}.csv;
+	[ -f $data ] && R -f ./scripts/heatmap.r --args $data ./$(plots_folder)/${unm}_bst_heatmap_uc_${initial}.pdf "BST, lf/lb, $unm, $initial initial" "Cores" "Update ratio"
+    done
 done
