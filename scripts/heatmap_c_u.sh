1ee2284b72d342695a1b76974b4a9aa5bbab9b94
Vasileios Trigonakis <vasileios.trigonakis@epfl.ch>
2014-02-20 16:23:50 +0100

    fix: was adding .cs instead of .csv suffix for some

--- scripts/heatmap_c_u.sh
+++ scripts/heatmap_c_u.sh
@@ -25,5 +25,5 @@ echo '#  ht';
 echo '#  sl';
 ./scripts/heatmap.sh ./bin/lb-sl ./bin/lf-sl u c -i${initial} -r${range} -d$duration | tee data/${unm}_sl_heatmap_uc_${initial}.csv
 echo '#  bst';
-./scripts/heatmap.sh ./bin/lb-bst2 ./bin/lf-bst-howley u c -i${initial} -r${range} -d$duration | tee data/${unm}_bst_heatmap_uc_${initial}.cs
+./scripts/heatmap.sh ./bin/lb-bst2 ./bin/lf-bst-howley u c -i${initial} -r${range} -d$duration | tee data/${unm}_bst_heatmap_uc_${initial}.csv
 done
