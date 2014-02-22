d7f4dad87101885aad8a65cc7fe074216be47e2e
Tudor David <tudor.david@epfl.ch>
2014-02-22 19:18:07 +0100

    heatmaps for scalability; 1 thread also measured; heatmaps now display the labels of the form x/y, where x - the lf throuhgput (or scalability) and y - the lb throughput (or scalability); data files with the actual throughputs also saved

--- scripts/cu_heatmaps_merge_pdf.sh
+++ scripts/cu_heatmaps_merge_pdf.sh
@@ -2,7 +2,7 @@
 
 structures="ll ht sl bst";
 unames="maglite lpd48core lpdxeon2680 parsasrv1.epfl.ch diassrv8 lpdpc4 ol-collab1"
-inits="256 1024 2048 8129 8192 65536"
+inits="256 1024 2048 8192 65536"
 
 
 plots_folder=./plots
@@ -15,6 +15,7 @@ do
     for structure in $structures;
     do
 	pdfs=$(ls -1 ${unm}_${structure}_heatmap_uc_*.pdf | sort -V);
+	pdfs_scal=$(ls -1 ${unm}_${structure}_heatmap_scal_*.pdf | sort -V);
 	if [ "$pdfs" != "" ];
 	then
 	    any_pdf=1;
@@ -22,6 +23,14 @@ do
 	    pdftk $pdfs cat output $all_sep;
 	    pdfnup --a4paper --nup 3x2 --outfile "${unm}_${structure}_heatmap_all.pdf" $all_sep;
 	fi;
+	if [ "$pdfs_scal" != "" ];
+	then
+	    any_pdf_scal=1;
+	    all_sep_scal="${unm}_${structure}_heatmap_all_sep_scal.pdf";
+	    pdftk ${pdfs_scal} cat output ${all_sep_scal};
+	    pdfnup --a4paper --nup 3x2 --outfile "${unm}_${structure}_heatmap_all_scal.pdf" ${all_sep_scal};
+	fi;
+
     done;
 
     if [ $any_pdf -eq 1 ];
@@ -29,6 +38,12 @@ do
 	all=$(ls ${unm}_*_heatmap_all.pdf);
 	pdftk $all cat output ${unm}_heatmap_all.pdf
     fi;
+    if [ $any_pdf_scal -eq 1 ];
+    then
+	all_scal=$(ls ${unm}_*_heatmap_all_scal.pdf);
+	pdftk ${all_scal} cat output ${unm}_heatmap_all_scal.pdf
+    fi;
+
 done
 
     # for initial in ${inits}
---
+++ scripts/heatmap_c_u_th.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+MAKE=make
+
+unm=$(uname -n);
+if [ $unm = "ol-collab1" ];
+then
+    MAKE=gmake
+fi;
+
+if [ $# -eq 0 ];		# pass any param to avoid compilation
+then
+    INIT=one GRANULARITY=GLOBAL_LOCK $MAKE -k tas
+    INIT=one $MAKE -k tas
+    INIT=one $MAKE -k lockfree
+fi
+
+inits="256 1024 2048 8192 65536"
+duration=2000;
+
+for initial in ${inits}
+do
+range=$((2*${initial}));
+
+echo "## initial: $initial";
+
+unm=$(uname -n);
+rm data/${unm}_*_heatmap_uc_*${initial}.csv
+echo '#  ll';
+./scripts/heatmap_th.sh ./bin/lb-ll ./bin/lf-ll u c -i${initial} -r${range} -d$duration | tee data/${unm}_ll_heatmap_uc_${initial}.csv
+cp data/temp1.txt data/${unm}_ll_heatmap_uc_lb_${initial}.csv
+cp data/temp2.txt data/${unm}_ll_heatmap_uc_lf_${initial}.csv
+cp data/temp3.txt data/${unm}_ll_heatmap_uc_frac_${initial}.csv
+cp data/scal_temp.txt data/${unm}_ll_heatmap_uc_scal_${initial}.csv
+cp data/scal_data.txt data/${unm}_ll_heatmap_uc_scal_frac_${initial}.csv
+echo '#  ht';
+./scripts/heatmap_th.sh ./bin/lb-ht_gl ./bin/lf-ht u c -i${initial} -r${range} -d$duration | tee data/${unm}_ht_heatmap_uc_${initial}.csv
+cp data/temp1.txt data/${unm}_ht_heatmap_uc_lb_${initial}.csv
+cp data/temp2.txt data/${unm}_ht_heatmap_uc_lf_${initial}.csv
+cp data/temp3.txt data/${unm}_ht_heatmap_uc_frac_${initial}.csv
+cp data/scal_temp.txt data/${unm}_ht_heatmap_uc_scal_${initial}.csv
+cp data/scal_data.txt data/${unm}_ht_heatmap_uc_scal_frac_${initial}.csv
+echo '#  sl';
+./scripts/heatmap_th.sh ./bin/lb-sl ./bin/lf-sl u c -i${initial} -r${range} -d$duration | tee data/${unm}_sl_heatmap_uc_${initial}.csv
+cp data/temp1.txt data/${unm}_sl_heatmap_uc_lb_${initial}.csv
+cp data/temp2.txt data/${unm}_sl_heatmap_uc_lf_${initial}.csv
+cp data/temp3.txt data/${unm}_sl_heatmap_uc_frac_${initial}.csv
+cp data/scal_temp.txt data/${unm}_sl_heatmap_uc_scal_${initial}.csv
+cp data/scal_data.txt data/${unm}_sl_heatmap_uc_scal_frac_${initial}.csv
+echo '#  bst';
+./scripts/heatmap_th.sh ./bin/lb-bst2 ./bin/lf-bst-howley u c -i${initial} -r${range} -d$duration | tee data/${unm}_bst_heatmap_uc_${initial}.csv
+cp data/temp1.txt data/${unm}_bst_heatmap_uc_lb_${initial}.csv
+cp data/temp2.txt data/${unm}_bst_heatmap_uc_lf_${initial}.csv
+cp data/temp3.txt data/${unm}_bst_heatmap_uc_frac_${initial}.csv
+cp data/scal_temp.txt data/${unm}_bst_heatmap_uc_scal_${initial}.csv
+cp data/scal_data.txt data/${unm}_bst_heatmap_uc_scal_frac_${initial}.csv
+done
