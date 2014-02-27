9cc3231ed4a876de6e52c208f69d8be4b3d6ffcc
Tudor David <tudor.david@epfl.ch>
2014-02-27 10:24:53 +0100

    added heatmap config file; added option to select the lf bst (ellen or howley) to be used in scripts

--- scripts/lat_distribution_all.sh
+++ scripts/lat_distribution_all.sh
@@ -8,6 +8,8 @@ then
     MAKE=gmake
 fi;
 
+source lat_distribution.config
+
 if [ $# -eq 0 ];		# pass any param to avoid compilation
 then
     LATENCY=2 INIT=one GRANULARITY=GLOBAL_LOCK $MAKE -k tas
@@ -21,5 +23,5 @@ fi
 ./scripts/lat_distribution.sh ${unm}_lat_lb_ht ./bin/lb-ht_gl -d1000 -f1024 -v1024
 ./scripts/lat_distribution.sh ${unm}_lat_lf_sl ./bin/lf-sl -d1000 -f1024 -v1024
 ./scripts/lat_distribution.sh ${unm}_lat_lb_sl ./bin/lb-sl -d1000 -f1024 -v1024
-./scripts/lat_distribution.sh ${unm}_lat_lf_bst ./bin/lf-bst-howley -d1000 -f1024 -v1024
+./scripts/lat_distribution.sh ${unm}_lat_lf_bst ./bin/${lftree} -d1000 -f1024 -v1024
 ./scripts/lat_distribution.sh ${unm}_lat_lb_bst ./bin/lb-bst2 -d1000 -f1024 -v1024
