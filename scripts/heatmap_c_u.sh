3c94f21dd1939094b640462550360d1734b1bc90
Tudor David <tudor.david@epfl.ch>
2014-02-28 11:12:39 +0100

    executables used for the tests can now be set in executables.config

--- scripts/heatmap_c_u.sh
+++ scripts/heatmap_c_u.sh
@@ -16,6 +16,7 @@ then
 fi
 
 source ./scripts/heatmap.config
+source ./scripts/executables.config
 
 inits="256 1024 2048 8192 65536"
 duration=2000;
@@ -29,11 +30,11 @@ echo "## initial: $initial";
 unm=$(uname -n);
 rm data/${unm}_*_heatmap_uc_${initial}.csv
 echo '#  ll';
-./scripts/heatmap.sh ./bin/lb-ll ./bin/lf-ll u c -i${initial} -r${range} -d$duration | tee data/${unm}_ll_heatmap_uc_${initial}.csv
+./scripts/heatmap.sh ${lb_ll} ${lf_ll} u c -i${initial} -r${range} -d$duration | tee data/${unm}_ll_heatmap_uc_${initial}.csv
 echo '#  ht';
-./scripts/heatmap.sh ./bin/lb-ht_gl ./bin/lf-ht u c -i${initial} -r${range} -d$duration | tee data/${unm}_ht_heatmap_uc_${initial}.csv
+./scripts/heatmap.sh ${lb_ht} ${lf_ht} u c -i${initial} -r${range} -d$duration | tee data/${unm}_ht_heatmap_uc_${initial}.csv
 echo '#  sl';
-./scripts/heatmap.sh ./bin/lb-sl ./bin/lf-sl u c -i${initial} -r${range} -d$duration | tee data/${unm}_sl_heatmap_uc_${initial}.csv
+./scripts/heatmap.sh ${lb_sl} ${lf_sl} u c -i${initial} -r${range} -d$duration | tee data/${unm}_sl_heatmap_uc_${initial}.csv
 echo '#  bst';
-./scripts/heatmap.sh ./bin/lb-bst2 ./bin/${lftree} u c -i${initial} -r${range} -d$duration | tee data/${unm}_bst_heatmap_uc_${initial}.csv
+./scripts/heatmap.sh ${lb_bst} ${lf_bst} u c -i${initial} -r${range} -d$duration | tee data/${unm}_bst_heatmap_uc_${initial}.csv
 done
--- scripts/heatmap_data.sh
+++ scripts/heatmap_data.sh
@@ -7,11 +7,12 @@ fi
 core=$1
 
 source ./scripts/heatmap.config
+source ./scripts/executables.config
 
 unm=$(uname -n);
 rm data/${unm}_*_heatmap_${core}.csv
-./scripts/heatmap.sh ./bin/lb-ll ./bin/lf-ll u s -n${core} -d1000 > data/${unm}_ll_heatmap_${core}.csv
-./scripts/heatmap.sh ./bin/lb-ht-gl ./bin/lf-ht u s -n${core} -d1000 > data/${unm}_ht_heatmap_${core}.csv
-./scripts/heatmap.sh ./bin/lb-sl ./bin/lf-sl u s -n${core} -d1000 > data/${unm}_sl_heatmap_${core}.csv
-./scripts/heatmap.sh ./bin/lb-bst2 ./bin/${lftree} u s -n${core} -d1000 > data/${unm}_bst_heatmap_${core}.csv
+./scripts/heatmap.sh ${lb_ll} ${lf_ll} u s -n${core} -d1000 > data/${unm}_ll_heatmap_${core}.csv
+./scripts/heatmap.sh ${lb_ht} ${lf_ht} u s -n${core} -d1000 > data/${unm}_ht_heatmap_${core}.csv
+./scripts/heatmap.sh ${lb_sl} ${lf_sl} u s -n${core} -d1000 > data/${unm}_sl_heatmap_${core}.csv
+./scripts/heatmap.sh ${lb_bst} ${lf_bst} u s -n${core} -d1000 > data/${unm}_bst_heatmap_${core}.csv
 
