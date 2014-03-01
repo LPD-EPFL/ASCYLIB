f87bfa3945611d4d2336ef215b14a3c09158eff3
Vasileios Trigonakis <vasileios.trigonakis@epfl.ch>
2014-03-01 19:58:23 +0100

    added do_ll, do_ht, do_sl, do_bst flags in configuration to enable/disable each data structure

--- scripts/heatmap_c_u_th.sh
+++ scripts/heatmap_c_u_th.sh
@@ -23,38 +23,53 @@ source ./scripts/executables.config
 
 for initial in ${inits}
 do
-range=$((2*${initial}));
+    range=$((2*${initial}));
 
-echo "## initial: $initial";
+    echo "## initial: $initial";
 
-unm=$(uname -n);
-rm data/${unm}_*_heatmap_uc_*${initial}.csv
-echo '#  ll';
-./scripts/heatmap_avg.sh ${lb_ll} ${lf_ll} u c -i${initial} -r${range} -d$duration | tee data/${unm}_ll_heatmap_uc_${initial}.csv
-cp data/temp1.txt data/${unm}_ll_heatmap_uc_lb_${initial}.csv
-cp data/temp2.txt data/${unm}_ll_heatmap_uc_lf_${initial}.csv
-cp data/temp3.txt data/${unm}_ll_heatmap_uc_frac_${initial}.csv
-cp data/scal_temp.txt data/${unm}_ll_heatmap_uc_scal_${initial}.csv
-cp data/scal_data.txt data/${unm}_ll_heatmap_uc_scal_frac_${initial}.csv
-echo '#  ht';
-./scripts/heatmap_avg.sh ${lb_ht} ${lf_ht} u c -i${initial} -r${range} -d$duration | tee data/${unm}_ht_heatmap_uc_${initial}.csv
-cp data/temp1.txt data/${unm}_ht_heatmap_uc_lb_${initial}.csv
-cp data/temp2.txt data/${unm}_ht_heatmap_uc_lf_${initial}.csv
-cp data/temp3.txt data/${unm}_ht_heatmap_uc_frac_${initial}.csv
-cp data/scal_temp.txt data/${unm}_ht_heatmap_uc_scal_${initial}.csv
-cp data/scal_data.txt data/${unm}_ht_heatmap_uc_scal_frac_${initial}.csv
-echo '#  sl';
-./scripts/heatmap_avg.sh ${lb_sl} ${lf_sl} u c -i${initial} -r${range} -d$duration | tee data/${unm}_sl_heatmap_uc_${initial}.csv
-cp data/temp1.txt data/${unm}_sl_heatmap_uc_lb_${initial}.csv
-cp data/temp2.txt data/${unm}_sl_heatmap_uc_lf_${initial}.csv
-cp data/temp3.txt data/${unm}_sl_heatmap_uc_frac_${initial}.csv
-cp data/scal_temp.txt data/${unm}_sl_heatmap_uc_scal_${initial}.csv
-cp data/scal_data.txt data/${unm}_sl_heatmap_uc_scal_frac_${initial}.csv
-echo '#  bst';
-./scripts/heatmap_avg.sh ${lb_bst} ${lf_bst} u c -i${initial} -r${range} -d$duration | tee data/${unm}_bst_heatmap_uc_${initial}.csv
-cp data/temp1.txt data/${unm}_bst_heatmap_uc_lb_${initial}.csv
-cp data/temp2.txt data/${unm}_bst_heatmap_uc_lf_${initial}.csv
-cp data/temp3.txt data/${unm}_bst_heatmap_uc_frac_${initial}.csv
-cp data/scal_temp.txt data/${unm}_bst_heatmap_uc_scal_${initial}.csv
-cp data/scal_data.txt data/${unm}_bst_heatmap_uc_scal_frac_${initial}.csv
+    unm=$(uname -n);
+    rm data/${unm}_*_heatmap_uc_*${initial}.csv
+    if [ $do_ll -eq 1 ];
+    then
+	echo '#  ll';
+	./scripts/heatmap_avg.sh "${lb_ll}" "${lf_ll}" u c -i${initial} -r${range} -d$duration | tee data/${unm}_ll_heatmap_uc_${initial}.csv
+	cp data/temp1.txt data/${unm}_ll_heatmap_uc_lb_${initial}.csv
+	cp data/temp2.txt data/${unm}_ll_heatmap_uc_lf_${initial}.csv
+	cp data/temp3.txt data/${unm}_ll_heatmap_uc_frac_${initial}.csv
+	cp data/scal_temp.txt data/${unm}_ll_heatmap_uc_scal_${initial}.csv
+	cp data/scal_data.txt data/${unm}_ll_heatmap_uc_scal_frac_${initial}.csv
+    fi
+
+    if [ $do_ht -eq 1 ];
+    then
+	echo '#  ht';
+	./scripts/heatmap_avg.sh "${lb_ht}" "${lf_ht}" u c -i${initial} -r${range} -d$duration | tee data/${unm}_ht_heatmap_uc_${initial}.csv
+	cp data/temp1.txt data/${unm}_ht_heatmap_uc_lb_${initial}.csv
+	cp data/temp2.txt data/${unm}_ht_heatmap_uc_lf_${initial}.csv
+	cp data/temp3.txt data/${unm}_ht_heatmap_uc_frac_${initial}.csv
+	cp data/scal_temp.txt data/${unm}_ht_heatmap_uc_scal_${initial}.csv
+	cp data/scal_data.txt data/${unm}_ht_heatmap_uc_scal_frac_${initial}.csv
+    fi
+
+    if [ $do_sl -eq 1 ];
+    then
+	echo '#  sl';
+	./scripts/heatmap_avg.sh "${lb_sl}" "${lf_sl}" u c -i${initial} -r${range} -d$duration | tee data/${unm}_sl_heatmap_uc_${initial}.csv
+	cp data/temp1.txt data/${unm}_sl_heatmap_uc_lb_${initial}.csv
+	cp data/temp2.txt data/${unm}_sl_heatmap_uc_lf_${initial}.csv
+	cp data/temp3.txt data/${unm}_sl_heatmap_uc_frac_${initial}.csv
+	cp data/scal_temp.txt data/${unm}_sl_heatmap_uc_scal_${initial}.csv
+	cp data/scal_data.txt data/${unm}_sl_heatmap_uc_scal_frac_${initial}.csv
+    fi
+
+    if [ $do_bst -eq 1 ];
+    then
+	echo '#  bst';
+	./scripts/heatmap_avg.sh "${lb_bst}" "${lf_bst}" u c -i${initial} -r${range} -d$duration | tee data/${unm}_bst_heatmap_uc_${initial}.csv
+	cp data/temp1.txt data/${unm}_bst_heatmap_uc_lb_${initial}.csv
+	cp data/temp2.txt data/${unm}_bst_heatmap_uc_lf_${initial}.csv
+	cp data/temp3.txt data/${unm}_bst_heatmap_uc_frac_${initial}.csv
+	cp data/scal_temp.txt data/${unm}_bst_heatmap_uc_scal_${initial}.csv
+	cp data/scal_data.txt data/${unm}_bst_heatmap_uc_scal_frac_${initial}.csv
+    fi
 done
--- scripts/heatmap_lat_all.sh
+++ scripts/heatmap_lat_all.sh
@@ -23,56 +23,71 @@ source ./scripts/executables.config
 
 for initial in ${inits}
 do
-range=$((2*${initial}));
+    range=$((2*${initial}));
 
-echo "## initial: $initial";
+    echo "## initial: $initial";
 
-unm=$(uname -n);
-rm data/${unm}_*_heatmap_uc_lat_*_${initial}.csv
-echo '#  ll';
-./scripts/heatmap_lat.sh ${lb_ll} ${lf_ll} u c -i${initial} -r${range} -d$duration
-cp data/lat_put_lb.txt data/${unm}_ll_heatmap_uc_lat_put_lb_${initial}.csv
-cp data/lat_put_lf.txt data/${unm}_ll_heatmap_uc_lat_put_lf_${initial}.csv
-cp data/lat_put_ratio.txt data/${unm}_ll_heatmap_uc_lat_put_ratio_${initial}.csv
-cp data/lat_get_lb.txt data/${unm}_ll_heatmap_uc_lat_get_lb_${initial}.csv
-cp data/lat_get_lf.txt data/${unm}_ll_heatmap_uc_lat_get_lf_${initial}.csv
-cp data/lat_get_ratio.txt data/${unm}_ll_heatmap_uc_lat_get_ratio_${initial}.csv
-cp data/lat_rem_lb.txt data/${unm}_ll_heatmap_uc_lat_rem_lb_${initial}.csv
-cp data/lat_rem_lf.txt data/${unm}_ll_heatmap_uc_lat_rem_lf_${initial}.csv
-cp data/lat_rem_ratio.txt data/${unm}_ll_heatmap_uc_lat_rem_ratio_${initial}.csv
-echo '#  ht';
-./scripts/heatmap_lat.sh ${lb_ht} ${lf_ht} u c -i${initial} -r${range} -d$duration
-cp data/lat_put_lb.txt data/${unm}_ht_heatmap_uc_lat_put_lb_${initial}.csv
-cp data/lat_put_lf.txt data/${unm}_ht_heatmap_uc_lat_put_lf_${initial}.csv
-cp data/lat_put_ratio.txt data/${unm}_ht_heatmap_uc_lat_put_ratio_${initial}.csv
-cp data/lat_get_lb.txt data/${unm}_ht_heatmap_uc_lat_get_lb_${initial}.csv
-cp data/lat_get_lf.txt data/${unm}_ht_heatmap_uc_lat_get_lf_${initial}.csv
-cp data/lat_get_ratio.txt data/${unm}_ht_heatmap_uc_lat_get_ratio_${initial}.csv
-cp data/lat_rem_lb.txt data/${unm}_ht_heatmap_uc_lat_rem_lb_${initial}.csv
-cp data/lat_rem_lf.txt data/${unm}_ht_heatmap_uc_lat_rem_lf_${initial}.csv
-cp data/lat_rem_ratio.txt data/${unm}_ht_heatmap_uc_lat_rem_ratio_${initial}.csv
-echo '#  sl';
-./scripts/heatmap_lat.sh ${lb_sl} ${lf_sl} u c -i${initial} -r${range} -d$duration
-cp data/lat_put_lb.txt data/${unm}_sl_heatmap_uc_lat_put_lb_${initial}.csv
-cp data/lat_put_lf.txt data/${unm}_sl_heatmap_uc_lat_put_lf_${initial}.csv
-cp data/lat_put_ratio.txt data/${unm}_sl_heatmap_uc_lat_put_ratio_${initial}.csv
-cp data/lat_get_lb.txt data/${unm}_sl_heatmap_uc_lat_get_lb_${initial}.csv
-cp data/lat_get_lf.txt data/${unm}_sl_heatmap_uc_lat_get_lf_${initial}.csv
-cp data/lat_get_ratio.txt data/${unm}_sl_heatmap_uc_lat_get_ratio_${initial}.csv
-cp data/lat_rem_lb.txt data/${unm}_sl_heatmap_uc_lat_rem_lb_${initial}.csv
-cp data/lat_rem_lf.txt data/${unm}_sl_heatmap_uc_lat_rem_lf_${initial}.csv
-cp data/lat_rem_ratio.txt data/${unm}_sl_heatmap_uc_lat_rem_ratio_${initial}.csv
-echo '#  bst';
-./scripts/heatmap_lat.sh ${lb_bst} ${lf_bst} u c -i${initial} -r${range} -d$duration
-cp data/lat_put_lb.txt data/${unm}_bst_heatmap_uc_lat_put_lb_${initial}.csv
-cp data/lat_put_lf.txt data/${unm}_bst_heatmap_uc_lat_put_lf_${initial}.csv
-cp data/lat_put_ratio.txt data/${unm}_bst_heatmap_uc_lat_put_ratio_${initial}.csv
-cp data/lat_get_lb.txt data/${unm}_bst_heatmap_uc_lat_get_lb_${initial}.csv
-cp data/lat_get_lf.txt data/${unm}_bst_heatmap_uc_lat_get_lf_${initial}.csv
-cp data/lat_get_ratio.txt data/${unm}_bst_heatmap_uc_lat_get_ratio_${initial}.csv
-cp data/lat_rem_lb.txt data/${unm}_bst_heatmap_uc_lat_rem_lb_${initial}.csv
-cp data/lat_rem_lf.txt data/${unm}_bst_heatmap_uc_lat_rem_lf_${initial}.csv
-cp data/lat_rem_ratio.txt data/${unm}_bst_heatmap_uc_lat_rem_ratio_${initial}.csv
+    unm=$(uname -n);
+    rm data/${unm}_*_heatmap_uc_lat_*_${initial}.csv
+    if [ $do_ll -eq 1 ];
+    then
+	echo '#  ll';
+	./scripts/heatmap_lat.sh "${lb_ll}" "${lf_ll}" u c -i${initial} -r${range} -d$duration
+	cp data/lat_put_lb.txt data/${unm}_ll_heatmap_uc_lat_put_lb_${initial}.csv
+	cp data/lat_put_lf.txt data/${unm}_ll_heatmap_uc_lat_put_lf_${initial}.csv
+	cp data/lat_put_ratio.txt data/${unm}_ll_heatmap_uc_lat_put_ratio_${initial}.csv
+	cp data/lat_get_lb.txt data/${unm}_ll_heatmap_uc_lat_get_lb_${initial}.csv
+	cp data/lat_get_lf.txt data/${unm}_ll_heatmap_uc_lat_get_lf_${initial}.csv
+	cp data/lat_get_ratio.txt data/${unm}_ll_heatmap_uc_lat_get_ratio_${initial}.csv
+	cp data/lat_rem_lb.txt data/${unm}_ll_heatmap_uc_lat_rem_lb_${initial}.csv
+	cp data/lat_rem_lf.txt data/${unm}_ll_heatmap_uc_lat_rem_lf_${initial}.csv
+	cp data/lat_rem_ratio.txt data/${unm}_ll_heatmap_uc_lat_rem_ratio_${initial}.csv
+    fi
+
+    if [ $do_ht -eq 1 ];
+    then
+	echo '#  ht';
+	./scripts/heatmap_lat.sh "${lb_ht}" "${lf_ht}" u c -i${initial} -r${range} -d$duration
+	cp data/lat_put_lb.txt data/${unm}_ht_heatmap_uc_lat_put_lb_${initial}.csv
+	cp data/lat_put_lf.txt data/${unm}_ht_heatmap_uc_lat_put_lf_${initial}.csv
+	cp data/lat_put_ratio.txt data/${unm}_ht_heatmap_uc_lat_put_ratio_${initial}.csv
+	cp data/lat_get_lb.txt data/${unm}_ht_heatmap_uc_lat_get_lb_${initial}.csv
+	cp data/lat_get_lf.txt data/${unm}_ht_heatmap_uc_lat_get_lf_${initial}.csv
+	cp data/lat_get_ratio.txt data/${unm}_ht_heatmap_uc_lat_get_ratio_${initial}.csv
+	cp data/lat_rem_lb.txt data/${unm}_ht_heatmap_uc_lat_rem_lb_${initial}.csv
+	cp data/lat_rem_lf.txt data/${unm}_ht_heatmap_uc_lat_rem_lf_${initial}.csv
+	cp data/lat_rem_ratio.txt data/${unm}_ht_heatmap_uc_lat_rem_ratio_${initial}.csv
+    fi
+
+    if [ $do_sl -eq 1 ];
+    then
+	echo '#  sl';
+	./scripts/heatmap_lat.sh "${lb_sl}" "${lf_sl}" u c -i${initial} -r${range} -d$duration
+	cp data/lat_put_lb.txt data/${unm}_sl_heatmap_uc_lat_put_lb_${initial}.csv
+	cp data/lat_put_lf.txt data/${unm}_sl_heatmap_uc_lat_put_lf_${initial}.csv
+	cp data/lat_put_ratio.txt data/${unm}_sl_heatmap_uc_lat_put_ratio_${initial}.csv
+	cp data/lat_get_lb.txt data/${unm}_sl_heatmap_uc_lat_get_lb_${initial}.csv
+	cp data/lat_get_lf.txt data/${unm}_sl_heatmap_uc_lat_get_lf_${initial}.csv
+	cp data/lat_get_ratio.txt data/${unm}_sl_heatmap_uc_lat_get_ratio_${initial}.csv
+	cp data/lat_rem_lb.txt data/${unm}_sl_heatmap_uc_lat_rem_lb_${initial}.csv
+	cp data/lat_rem_lf.txt data/${unm}_sl_heatmap_uc_lat_rem_lf_${initial}.csv
+	cp data/lat_rem_ratio.txt data/${unm}_sl_heatmap_uc_lat_rem_ratio_${initial}.csv
+    fi
+
+    if [ $do_bst -eq 1 ];
+    then
+	echo '#  bst';
+	./scripts/heatmap_lat.sh "${lb_bst}" "${lf_bst}" u c -i${initial} -r${range} -d$duration
+	cp data/lat_put_lb.txt data/${unm}_bst_heatmap_uc_lat_put_lb_${initial}.csv
+	cp data/lat_put_lf.txt data/${unm}_bst_heatmap_uc_lat_put_lf_${initial}.csv
+	cp data/lat_put_ratio.txt data/${unm}_bst_heatmap_uc_lat_put_ratio_${initial}.csv
+	cp data/lat_get_lb.txt data/${unm}_bst_heatmap_uc_lat_get_lb_${initial}.csv
+	cp data/lat_get_lf.txt data/${unm}_bst_heatmap_uc_lat_get_lf_${initial}.csv
+	cp data/lat_get_ratio.txt data/${unm}_bst_heatmap_uc_lat_get_ratio_${initial}.csv
+	cp data/lat_rem_lb.txt data/${unm}_bst_heatmap_uc_lat_rem_lb_${initial}.csv
+	cp data/lat_rem_lf.txt data/${unm}_bst_heatmap_uc_lat_rem_lf_${initial}.csv
+	cp data/lat_rem_ratio.txt data/${unm}_bst_heatmap_uc_lat_rem_ratio_${initial}.csv
+    fi
 done
 
 #remove data for u0 from put and rem files
