fd2f84a08ead9e27d4876c5fc41549d6a7123aab
Vasileios Trigonakis <vasileios.trigonakis@epfl.ch>
2014-03-25 19:15:08 +0100

    scripts similar to scalability.sh for measuring latency (with compilations of LATENCY=1)

---
+++ scripts/latency8.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+cores=$1;
+shift;
+
+source scripts/lock_exec;
+source scripts/config;
+
+prog1="$1";
+shift;
+prog2="$1";
+shift;
+prog3="$1";
+shift;
+prog4="$1";
+shift;
+prog5="$1";
+shift;
+prog6="$1";
+shift;
+prog7="$1";
+shift;
+prog8="$1";
+shift;
+params="$@";
+
+printf "#   %-42s%-42s%-42s%-42s%-42s%-42s%-42s%-42s\n" "$prog1" "$prog2" "$prog3" "$prog4" "$prog5" "$prog6" "$prog7" "$prog8";
+
+echo "#co get_s  get_f put_s  put_f rem_s  rem_f get_s  get_f put_s  put_f rem_s  rem_f get_s  get_f put_s  put_f rem_s  rem_f get_s  get_f put_s  put_f rem_s  rem_f get_s  get_f put_s  put_f rem_s  rem_f get_s  get_f put_s  put_f rem_s  rem_f get_s  get_f put_s  put_f rem_s  rem_f get_s  get_f put_s  put_f rem_s  rem_f";
+
+d=0;
+for c in 1 $cores
+do
+    if [ $c -eq 1 ];
+    then
+	if [ $d -eq 1 ];
+	then
+	    continue;
+	fi;
+    fi;
+    d=1;
+
+    printf "%-4d" $c;
+    prog=$prog1;
+
+    thr=$($run_script ./$prog $params -n$c | grep "#thread" -A1 | tail -n1 | awk '{$1=""; print}');
+    printf "%-7d%-7d%-7d%-7d%-7d%-7d" $thr;
+
+    prog=$prog2;
+
+    thr=$($run_script ./$prog $params -n$c | grep "#thread" -A1 | tail -n1 | awk '{$1=""; print}');
+    printf "%-7d%-7d%-7d%-7d%-7d%-7d" $thr;
+
+    prog=$prog3;
+
+    thr=$($run_script ./$prog $params -n$c | grep "#thread" -A1 | tail -n1 | awk '{$1=""; print}');
+    printf "%-7d%-7d%-7d%-7d%-7d%-7d" $thr;
+
+    prog=$prog4;
+
+    thr=$($run_script ./$prog $params -n$c | grep "#thread" -A1 | tail -n1 | awk '{$1=""; print}');
+    printf "%-7d%-7d%-7d%-7d%-7d%-7d" $thr;
+
+    prog=$prog5;
+
+    thr=$($run_script ./$prog $params -n$c | grep "#thread" -A1 | tail -n1 | awk '{$1=""; print}');
+    printf "%-7d%-7d%-7d%-7d%-7d%-7d" $thr;
+
+    prog=$prog6;
+
+    thr=$($run_script ./$prog $params -n$c | grep "#thread" -A1 | tail -n1 | awk '{$1=""; print}');
+    printf "%-7d%-7d%-7d%-7d%-7d%-7d" $thr;
+
+    prog=$prog7;
+
+    thr=$($run_script ./$prog $params -n$c | grep "#thread" -A1 | tail -n1 | awk '{$1=""; print}');
+    printf "%-7d%-7d%-7d%-7d%-7d%-7d" $thr;
+
+    prog=$prog8;
+
+    thr=$($run_script ./$prog $params -n$c | grep "#thread" -A1 | tail -n1 | awk '{$1=""; print}');
+    printf "%-7d%-7d%-7d%-7d%-7d%-7d" $thr;
+
+
+    echo "";
+done;
+
+source scripts/unlock_exec;
