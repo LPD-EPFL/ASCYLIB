fad6b662f172454b702184bba7b4da1beb2d9e58
Vasileios Trigonakis <vasileios.trigonakis@epfl.ch>
2014-03-26 15:39:25 +0100

    run N reps and keep the min/max/median respectivelly

---
+++ scripts/run_rep_max.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source scripts/config;
+
+reps=$1;
+shift;
+prog="$1";
+shift;
+params="$@";
+
+un=$(uname -n);
+tmp=data/run_rep_max.${un}.tmp
+printf "" > $tmp;
+
+for r in $(seq 1 1 $reps);
+do
+    $run_script ./$prog $params | grep "#txs" | cut -d'(' -f2 | cut -d. -f1 >> $tmp;
+done;
+
+sort -n $tmp | tail -n1;
+
---
+++ scripts/run_rep_med.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source scripts/config
+
+reps=$1;
+shift;
+prog="$1";
+shift;
+params="$@";
+
+un=$(uname -n);
+tmp=data/run_rep_med.${un}.tmp
+printf "" > $tmp;
+
+for r in $(seq 1 1 $reps);
+do
+    $run_script ./$prog $params | grep "#txs" | cut -d'(' -f2 | cut -d. -f1 >> $tmp;
+done;
+
+med_idx=$(echo "$reps/2" | bc);
+sort -n $tmp | head -${med_idx} | tail -n1;
+
