e893eeb4df711e62ff41b348cdba566437de4ed1
Vasileios Trigonakis <vasileios.trigonakis@epfl.ch>
2014-03-27 09:31:35 +0100

    added the estimated time to complete the experiment + reduced the time and reps

--- scripts/osdi/run_scy1.sh
+++ scripts/osdi/run_scy1.sh
@@ -4,9 +4,9 @@ cores_lat_dist="5 10 20";
 LATENCY_TYPE=2			# 2 or 3
 LATENCY_POINTS=16384
 
-reps=10;
+reps=8;
 keep=median 			# min, median, or max
-duration=2000;
+duration=1000;
 initials="1024 4096 8192";
 updates="0 1 10 20 50";
 cores="all";
@@ -19,6 +19,15 @@ ub="bin/$un";
 mkdir $ub;
 
 
+ll_num=8;
+ht_num=9;
+i_num=$(echo $initials | wc -w);
+u_num=$(echo $updates | wc -w);
+source scripts/config;
+c_num=$max_cores;
+est_time=$(echo "2*${i_num}*${u_num}*(${ll_num}+${ht_num})*${reps}*(${duration}/1000)*${c_num}/3600" | bc -l);
+printf "## Estimated time for the experiment (hours): %.3f\n" $est_time;
+sleep 2;
 
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
 echo "~~~~~~~~~~~~ ~~~~~~~~~~~~ Throughput";
