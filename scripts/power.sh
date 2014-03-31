87ce55402529f5c3acb46788c8661c4465b73906
Vasileios Trigonakis <vasileios.trigonakis@epfl.ch>
2014-03-31 09:34:09 +0200

    power.sh : print thr / thr/w / pow /eop

---
+++ scripts/power.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+cores=$1;
+shift;
+
+reps=$1;
+shift;
+
+source scripts/lock_exec;
+source scripts/config;
+source scripts/help;
+
+result_type=$1;
+
+if [ "$result_type" = "max" ];
+then
+    run_script="./scripts/run_rep_pow_max.sh $reps";
+    echo "# Result from $reps repetitions: max";
+    shift;
+
+elif [ "$result_type" = "min" ];
+then
+    run_script="./scripts/run_rep_pow_min.sh $reps";
+    echo "# Result from $reps repetitions: min";
+    shift;
+elif [ "$result_type" = "median" ];
+then
+    run_script="./scripts/run_rep_pow_med.sh $reps";
+    echo "# Result from $reps repetitions: median";
+    shift;
+else
+    run_script="./scripts/run_rep_pow_max.sh $reps";
+    echo "# Result from $reps repetitions: max (default). Available: min, max, median";
+fi;
+
+run_script="sudo $run_script";
+
+progs="$1";
+shift;
+progs_num=$(echo $progs | wc -w);
+params="$@";
+
+print_n "#   " "%-46s" "$progs" "\n"
+
+print_rep "#co " $progs_num "Thrput     Thrput/W   Power      uJ/op        " "\n";
+
+d=0;
+for c in 1 $cores
+do
+    if [ $c -eq 1 ];
+    then
+	if [ $d -eq 1 ];
+	then
+	    continue;
+	fi;
+    fi;
+    d=1;
+
+    printf "%-4d" $c;
+    i=0;
+    for p in $progs;
+    do
+	i=$(($i+1));
+	res=$($run_script ./$p $params -n$c);
+	printf "%-11d%-10.2f%-12.6f%-13.6f" $res
+    done;     
+    echo "";
+done;
+
+source scripts/unlock_exec;
---
+++ scripts/run_rep_pow_min.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source scripts/config;
+
+reps=$1;
+shift;
+prog="$1";
+shift;
+params="$@";
+
+un=$(uname -n);
+tmp=/tmp/run_rep_pow_max.${un}.tmp
+printf '' > $tmp;
+
+for r in $(seq 1 1 $reps);
+do
+    out=$($run_script ./$prog $params);
+    thr=$(echo "$out" | grep "#txs" | cut -d'(' -f2 | cut -d. -f1);
+    pow=$(echo "$out" | awk '/#Total Power Corrected/ { print $5 };' );
+    ppw=$(echo "$thr/$pow" | bc -l);
+    eop=$(echo "$out" | awk '/#Energy per Operation/ { print $8 }' | sed 's/)//g');
+
+    printf "%-10d%-10.2f %f %f \n" $thr $ppw $pow $eop >> $tmp;
+done;
+
+sort -n $tmp | head -n1;
+
