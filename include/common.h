b51fc2f6c7e1d7c62a5814de27a3dc6f0fa10bff
Vasileios Trigonakis <vasileios.trigonakis@epfl.ch>
2014-04-08 09:29:55 +0200

    added static assert

--- include/common.h
+++ include/common.h
@@ -10,6 +10,8 @@
 #define XSTR(s)                         STR(s)
 #define STR(s)                          #s
 
+#define STATIC_ASSERT(a, msg)           _Static_assert ((a), msg);
+
 typedef intptr_t skey_t;
 typedef intptr_t sval_t;
 
