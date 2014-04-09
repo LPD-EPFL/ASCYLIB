ae95e277a151516a20cccfa867f6d6117706c3e2
Tudor David <tudor.david@epfl.ch>
2014-04-09 16:51:12 +0200

    updated script

--- scripts/osdi/generate_graphs/modify_extremes.sh
+++ scripts/osdi/generate_graphs/modify_extremes.sh
@@ -2,8 +2,8 @@
 
 for f in data/extremes_ll*
 do
-    title=$(grep "throughput" ${f});
-    echo "${title}" ratio > data/temp
+    title=$(grep "throughput" ${f} | awk '{$6="ratio"; print}');
+    echo "${title}" > data/temp
     th=$(grep high ${f} | grep seq | cut -d' ' -f4); 
     grep high ${f} | awk '{$6=$4/'${th}'; print}' >> data/temp
     th=$(grep low ${f} | grep seq | cut -d' ' -f4); 
@@ -12,8 +12,8 @@ do
 done
 for f in data/extremes_sl*
 do
-    title=$(grep "throughput" ${f});
-    echo "${title}" ratio > data/temp
+    title=$(grep "throughput" ${f} | awk '{$6="ratio"; print}');
+    echo "${title}" > data/temp
     th=$(grep high ${f} | grep seq | cut -d' ' -f4); 
     grep high ${f} | awk '{$6=$4/'${th}'; print}' >> data/temp
     th=$(grep low ${f} | grep seq | cut -d' ' -f4); 
@@ -22,8 +22,8 @@ do
 done
 for f in data/extremes_ht*
 do
-    title=$(grep "throughput" ${f});
-    echo "${title}" ratio > data/temp
+    title=$(grep "throughput" ${f} | awk '{$6="ratio"; print}');
+    echo "${title}" > data/temp
     th=$(grep high ${f} | grep seq | cut -d' ' -f4); 
     grep high ${f} | awk '{$6=$4/'${th}'; print}' >> data/temp
     th=$(grep low ${f} | grep seq | cut -d' ' -f4); 
@@ -32,19 +32,21 @@ do
 done
 for f in data/extremes_bst*
 do
-    title=$(grep "throughput" ${f});
-    echo "${title}" ratio > data/temp
+    title=$(grep "throughput" ${f} | awk '{$6="ratio"; print}');
+    echo "${title}" > data/temp
     thi=$(grep high ${f} | grep "seq-int" | cut -d' ' -f4); 
     the=$(grep high ${f} | grep "seq-ext" | cut -d' ' -f4); 
     th=${thi}
-    if [ ${thi} -lt ${the} ]; then
+    gt=$(echo $the'>'$thi | bc -l)
+    if [ $gt -eq 1 ]; then
         th=${the}
     fi
     grep high ${f} | awk '{$6=$4/'${th}'; print}' >> data/temp
     thi=$(grep low ${f} | grep "seq-int" | cut -d' ' -f4); 
     the=$(grep low ${f} | grep "seq-ext" | cut -d' ' -f4); 
     th=${thi}
-    if [ ${thi} -lt ${the} ]; then
+    gt=$(echo $the'>'$thi | bc -l)
+    if [ $gt -eq 1 ]; then
         th=${the}
     fi
     grep low ${f} | awk '{$6=$4/'${th}'; print}' >> data/temp
