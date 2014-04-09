5f3ab8f2c1c6a5e2bb423b3a2a76e9d99c7b1bf2
Tudor David <tudor.david@epfl.ch>
2014-04-09 16:23:08 +0200

    script to add rario to sequential to test files

--- scripts/osdi/generate_graphs/modify_extremes.sh
+++ scripts/osdi/generate_graphs/modify_extremes.sh
@@ -1,11 +1,53 @@
 #!/bin/bash
 
-for f in data/extremes*
+for f in data/extremes_ll*
 do
+    title=$(grep "throughput" ${f});
+    echo "${title}" ratio > data/temp
     th=$(grep high ${f} | grep seq | cut -d' ' -f4); 
-    grep high ${f} | awk '{$4=$4/'${th}'; print}' > data/temp
+    grep high ${f} | awk '{$6=$4/'${th}'; print}' >> data/temp
     th=$(grep low ${f} | grep seq | cut -d' ' -f4); 
-    grep low ${f} | awk '{$4=$4/'${th}'; print}' >> data/temp
+    grep low ${f} | awk '{$6=$4/'${th}'; print}' >> data/temp
+    cp data/temp ${f}
+done
+for f in data/extremes_sl*
+do
+    title=$(grep "throughput" ${f});
+    echo "${title}" ratio > data/temp
+    th=$(grep high ${f} | grep seq | cut -d' ' -f4); 
+    grep high ${f} | awk '{$6=$4/'${th}'; print}' >> data/temp
+    th=$(grep low ${f} | grep seq | cut -d' ' -f4); 
+    grep low ${f} | awk '{$6=$4/'${th}'; print}' >> data/temp
+    cp data/temp ${f}
+done
+for f in data/extremes_ht*
+do
+    title=$(grep "throughput" ${f});
+    echo "${title}" ratio > data/temp
+    th=$(grep high ${f} | grep seq | cut -d' ' -f4); 
+    grep high ${f} | awk '{$6=$4/'${th}'; print}' >> data/temp
+    th=$(grep low ${f} | grep seq | cut -d' ' -f4); 
+    grep low ${f} | awk '{$6=$4/'${th}'; print}' >> data/temp
+    cp data/temp ${f}
+done
+for f in data/extremes_bst*
+do
+    title=$(grep "throughput" ${f});
+    echo "${title}" ratio > data/temp
+    thi=$(grep high ${f} | grep "seq-int" | cut -d' ' -f4); 
+    the=$(grep high ${f} | grep "seq-ext" | cut -d' ' -f4); 
+    th=${thi}
+    if [ ${thi} -lt ${the} ]; then
+        th=${the}
+    fi
+    grep high ${f} | awk '{$6=$4/'${th}'; print}' >> data/temp
+    thi=$(grep low ${f} | grep "seq-int" | cut -d' ' -f4); 
+    the=$(grep low ${f} | grep "seq-ext" | cut -d' ' -f4); 
+    th=${thi}
+    if [ ${thi} -lt ${the} ]; then
+        th=${the}
+    fi
+    grep low ${f} | awk '{$6=$4/'${th}'; print}' >> data/temp
     cp data/temp ${f}
 done
 
