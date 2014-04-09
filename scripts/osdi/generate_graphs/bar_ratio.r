c7d43abaf9b79a22dcc08d12690d447ba88dd44e
Tudor David <tudor.david@epfl.ch>
2014-04-09 16:34:42 +0200

    added ratio bar graph

---
+++ scripts/osdi/generate_graphs/bar_ratio.r
@@ -0,0 +1,30 @@
+library("ggplot2")
+library("scales")
+library("grid")
+library("gridExtra")
+args <- commandArgs()
+mydata=read.csv(args[5],sep=" ")
+
+g_legend <- function(p){
+   tmp <- ggplot_gtable(ggplot_build(p))
+   leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+   legend <- tmp$grobs[[leg]]
+   return(legend)
+}
+
+high=subset(mydata,experiment=="high")
+low=subset(mydata,experiment=="low")
+theme_set(theme_bw())
+ theme_update(plot.margin = unit(c(0,0,0,0), "cm"))
+ mvh=max(high$ratio)*1.4
+ mvl=max(low$ratio)*1.4
+p1=ggplot(data=high, aes(x=machine, y=ratio, fill=structure)) +xlab("")+ylab("Ratio v. sequential") +geom_bar(stat="identity", colour="black",position=position_dodge())+ggtitle("High contention")+geom_text(angle = 90, aes(label = paste(sprintf("%.1f", scalability), "", sep=""), y =ratio+mvh/9), size = 3.7, position = position_dodge(width=0.9)) +scale_fill_brewer(palette="OrRd") + scale_x_discrete(expand = c(0, 0)) + scale_y_continuous(expand = c(0, 0),limits= c(0,mvh)) + theme(legend.key.size = unit(0.45, "cm"))+theme(legend.text = element_text(size = 13))+theme(legend.title = element_text(size = 13))+theme(axis.text.y = element_text(size = )) + theme(axis.text.x = element_text(size = 12)) + opts( panel.grid.major = theme_blank(),panel.grid.minor = theme_blank())  + theme(panel.border= element_rect(colour="black"))
+p2=ggplot(data=low, aes(x=machine, y=ratio, fill=structure))+ggtitle("Low contention") +xlab("")+ylab("Ratio v. sequential") +geom_bar(stat="identity", colour="black",position=position_dodge())+geom_text(angle = 90, aes(label = paste(sprintf("%.1f", scalability), "", sep=""), y =ratio+mvl/9), size = 3.7, position = position_dodge(width=0.9)) +scale_fill_brewer(palette="OrRd") + scale_x_discrete(expand = c(0, 0)) + scale_y_continuous(expand = c(0, 0),limits= c(0,mvl))+ theme(axis.text.y = element_text(size = )) + theme(axis.text.x = element_text(size = 12)) +  opts( panel.grid.major = theme_blank(),panel.grid.minor = theme_blank()) + theme(panel.border= element_rect(colour="black"))
+
+
+leg<-g_legend(p1)
+dev.off()
+pdf(file=args[7],height=2,width=14)
+grid.arrange(p1+theme(legend.position="none"),p2+theme(legend.position="none"),leg,ncol=3,widths=c(31/70,31/70,8/70))
+#print(p1 + scale_x_discrete(expand = c(0, 0)) + scale_y_discrete(expand = c(0, 0)))
+dev.off()
