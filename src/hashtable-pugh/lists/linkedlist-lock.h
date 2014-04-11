8602a64f4cda06a61237346a50cca5f4caa87835
Vasileios Trigonakis <vasileios.trigonakis@epfl.ch>
2014-04-11 08:37:14 +0200

    padding of the node_t to 32bytes

--- src/hashtable-pugh/lists/linkedlist-lock.h
+++ src/hashtable-pugh/lists/linkedlist-lock.h
@@ -58,6 +58,7 @@ typedef ALIGNED(32) volatile struct node_l
   skey_t key;
   sval_t val;
   volatile struct node_l* next;
+  uint8_t padding32[8];
 #if !defined(LL_GLOBAL_LOCK)
   volatile ptlock_t lock;
 #endif
