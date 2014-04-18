89d0cd1b48959906c27df9473a20103b4b09d3c1
Vasileios Trigonakis <vasileios.trigonakis@epfl.ch>
2014-04-18 18:16:09 +0200

    settings for tilera in ssmem

--- external/include/ssmem.h
+++ external/include/ssmem.h
@@ -12,9 +12,9 @@
 #if defined(__sparc__)
 #  define SSMEM_GC_FREE_SET_SIZE 16379
 #else
-#  define SSMEM_GC_FREE_SET_SIZE 507
+#  define SSMEM_GC_FREE_SET_SIZE 123
 #endif
-#define SSMEM_DEFAULT_MEM_SIZE (32 * 1024 * 1024L)
+#define SSMEM_DEFAULT_MEM_SIZE (256 * 1024L)
 
 /* **************************************************************************************** */
 /* help definitions */
