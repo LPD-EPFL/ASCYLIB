c2849950759747fa32320b3c134d485d05b42d36
Igor Zablotchi <igor.zablotchi@epfl.ch>
2014-09-25 03:35:16 +0200

    Fixed test script

--- scripts/run_sl_levels_test.sh
+++ scripts/run_sl_levels_test.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-out_file="data/sl_28_levels_"$(date | gawk '// {print $2"_"$3}');
+out_file="../data/sl_28_levels_"$(date | gawk '// {print $2"_"$3}');
 echo "Will write results in"$out_file;
 updates="0 1 10";
+iterations="1 2 3 4";
 test="../bin/lb-sl_pugh"
 
 touch $out_file;
@@ -12,10 +13,11 @@ touch $out_file;
 
 for u in $updates
 do
-	settings="-n19 -d30000 -i200000000 -u$u";
+	settings="-n19 -d20000 -i200000000 -u$u";
 	echo $settings | tee -a $out_file;
-	for i in "1 2 3 4 5"
+	for i in $iterations
 	do
 		$test $settings | grep Mops | tee -a $out_file;
+#		echo "$u iteration $i"
 	done;
 done;
