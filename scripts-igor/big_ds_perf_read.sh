f4ff6bb25544ed67f431b80bf1e14466db7fc180
Igor Zablotchi <igor.zablotchi@epfl.ch>
2015-05-29 13:35:00 +0200

    Created results folder; gathered results from big data structures

--- scripts-igor/big_ds_perf_read.sh
+++ scripts-igor/big_ds_perf_read.sh
@@ -5,28 +5,6 @@ HT_SIZES="8192 32768 65536 262144 524288 2097152 4194304 33554432 134217728 2684
 SKIPLISTS="lb-sl_pugh lb-sl_herlihy lf-sl_fraser"
 HTS="lb-ht_java"
 
-cd ..; 
-make clean; rm bin/*;
-make htjava lfsl_fraser lbsl_pugh lbsl_herlihy_lb INIT_SEQ=1;
-
-for sl in $SKIPLISTS;
-do
-    for size in $SL_SIZES;
-    do
-        echo $sl - $size;
-        ./bin/$sl -n16 -u100 -i$size | tee -a big_perf_write;
-    done;
-done;
-
-for ht in $HTS;
-do
-    for size in $HT_SIZES;
-    do
-        echo $ht - $size;
-        ./bin/$ht -n16 -u100 -i$size | tee -a big_perf_write;
-    done;
-done;
-
 cd ..;
 make clean; rm bin/*;
 make htjava lfsl_fraser lbsl_pugh lbsl_herlihy_lb INIT_SEQ=1 SKEW9010=1
